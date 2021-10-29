000bb31fb263ad2c764efced7b3b0b6233a1e0fc
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-29 14:05:43 +0200

    Validation for media and reviews added

---
+++ src/services/validation.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+//================ MEDIA VALIDATION ===================
+export const mediaValidation = [
+  body("Title")
+    .isLength({ min: 1 })
+    .withMessage("Title has to be minimum 1 character"),
+  body("Year")
+    .isLength({ min: 4 })
+    .withMessage("Year has to be minimum 4 characters"),
+  body("Type")
+    .isLength({ min: 5 })
+    .withMessage("Type has to be minimum 5 characters"),
+];
+
+//================ REVIEWS VALIDATION ===================
+export const reviewsValidation = [
+  body("comment")
+    .isLength({ min: 10 })
+    .withMessage("The Comment has to be minimum 10 character"),
+  body("rate")
+    .isFloat({ min: 1, max: 5 })
+    .withMessage("The rate has to be a numeric value between 1 and 5."),
+];

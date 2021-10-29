c7f17e6e56487fd55b4f0e1023cc3fb6ef091da0
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-29 17:13:29 +0200

    bug fix

--- src/lib/pdfMakeTools.js
+++ src/lib/pdfMakeTools.js
@@ -1,31 +1,38 @@
 import PdfPrinter from "pdfmake";
 
-export const getMediaPDFReadableStream = async (filteredMedia, singleMediaReviews) => {
-    const printer = new PdfPrinter(fonts);
-    const base64Poster = await turnToBase64Format(filteredMedia.Poster);
-  
-    const docDefinition = {
-      content: [
-        {
-          text: `${filteredMedia.Title} (${filteredMedia.Year}) - ${filteredMedia.Type}`,
-          style: "header",
-        },
-        "\n\n",
-        { image: base64Poster, width: 250, style: "centerMe" },
-        "\n\n\n",
-      ],
-      styles: {
-        header: {
-          fontSize: 18,
-          bold: true,
-          alignment: "center",
-        },
-        subHeader: {
-          fontSize: 16,
-          bold: true,
-        },
-        centerMe: {
-          alignment: "center",
-        },
+const getMediaPDFReadableStream = async (
+  filteredMedia,
+  singleMediaReviews
+) => {
+  const printer = new PdfPrinter(fonts);
+  const base64Poster = await turnToBase64Format(filteredMedia.Poster);
+
+  const docDefinition = {
+    content: [
+      {
+        text: `${filteredMedia.Title} (${filteredMedia.Year}) - ${filteredMedia.Type}`,
+        style: "header",
       },
-    };
+      "\n\n",
+      { image: base64Poster, width: 250, style: "centerMe" },
+      "\n\n\n",
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        alignment: "center",
+      },
+      subHeader: {
+        fontSize: 16,
+        bold: true,
+      },
+      centerMe: {
+        alignment: "center",
+      },
+    },
+  };
+};
+
+
+export default getMediaPDFReadableStream
